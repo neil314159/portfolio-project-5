46f6741bcb82d2c1e4a29aeda466bcccc7fce9e3
neil314159 <neil@neilboland.com>
2022-08-07 02:06:41 +0000

    Install crispy forms and django-htmx packages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.views import generic, View
+from .models import BlogPost, BlogPostCategory
 # Create your views here.
 
 
@@ -8,3 +9,25 @@ def blog(request):
     """ A view to return the blog"""
 
     return render(request, 'blog/blogindex.html')
+
+
+class BlogCategoryList(generic.ListView):
+    """Takes GET request, returns articles by category"""
+    model = BlogPostCategory
+    template_name = "category.html"
+    context_object_name = 'categorylist'
+
+    def get_queryset(self):
+        content = {
+            'cat': self.kwargs['category'],
+            'posts': BlogPost.objects
+            .filter(category__name=self.kwargs['category'])
+        }
+        return content
+
+
+def blog_category_list(request):
+    """ Creates a list of categories to be used by dropdown menu"""
+    blog_category_list = Category.objects.all()
+    context = {"review_category_list": review_category_list, }
+    return context
