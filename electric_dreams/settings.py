2e08a3696c5840051f54567cb34231faefa9360b
neil314159 <neil@neilboland.com>
2022-08-07 21:17:23 +0000

    Add crispy forms setting to settings.py

--- electric_dreams/settings.py
+++ electric_dreams/settings.py
@@ -34,6 +34,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['electric-dreams.herokuapp.com', 'localhost']
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,11 +117,16 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
