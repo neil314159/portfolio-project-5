50ddd9b36edb35a2ec08addd78453dda809b1516
neil314159 <neil@neilboland.com>
2022-08-07 00:39:48 +0000

    Create URL structure and views for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def blog(request):
+    """ A view to return the blog"""
+
+    return render(request, 'blog/blogindex.html')
