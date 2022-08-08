81ae0d299ffcf3ade615b4aa2272abf6d09428b0
neil314159 <neil@neilboland.com>
2022-08-08 00:47:04 +0000

    Extended AbstractUSer model with extra custom fields for addresses

--- profiles/models.py
+++ profiles/models.py
@@ -4,7 +4,17 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
-    pass
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
