e7f79760ae85982306889c96fee33b9e4c1125a6
neil314159 <neil@neilboland.com>
2022-08-08 19:11:17 +0000

    Add webhook email settings

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 from .models import Order, OrderLineItem
 from products.models import Product
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 import json
 import time
