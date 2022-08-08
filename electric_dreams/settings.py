c18a7a9e1119af4bc808939427fef68e7a72f3da
neil314159 <neil@neilboland.com>
2022-08-08 22:15:25 +0000

    Refactor bag template into multiple files

--- electric_dreams/settings.py
+++ electric_dreams/settings.py
@@ -201,11 +201,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'profiles.CustomUser'
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -24,8 +25,16 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    
+    image= CloudinaryField(
+        'image',
+        blank=True,
+        transformation={
+            'width': '1000',
+            'height': '1000',
+            'crop': 'fill',
+            'gravity': "auto"},
+        default=("https://res.cloudinary.com/dpsodnurd/image/upload/v1652618629/zhtctppqjky78q8h760n.jpg"))
 
     def __str__(self):
         return self.name
