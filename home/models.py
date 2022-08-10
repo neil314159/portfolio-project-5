aa50e59812603073041b8faa1c29463a12021b8b
neil314159 <neil@neilboland.com>
2022-08-10 20:00:53 +0000

    Add model for contact form messages

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ArtRequestFormMessage(models.Model):
+    '''
+    custom art requests messages
+    '''
+    message_text = models.TextField(blank=False, null=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+
+
