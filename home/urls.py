ba34a83d4a46a1db37acb2fae80a84bc105b7797
neil314159 <neil@neilboland.com>
2022-08-11 03:33:49 +0000

    Remove image gallery as too simialr to product view, not enough utility

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('privacy/', views.privacy_policy, name='privacy'),
-    path('gallery/', views.gallery, name='gallery'),
     path('artrequest/', views.artwork_request, name='artrequest'),
 ]
