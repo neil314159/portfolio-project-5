c39c02e944c0a61a1ab51307c18b32bffd260b60
neil314159 <neil@neilboland.com>
2022-08-12 12:33:20 +0000

    Add order detail page and style order management page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path('', views.get_dashboard, name='dashboard_view'),
     path('orders/', views.all_orders, name='manage_orders'),
-    path('order_details/<order_number>', views.order_details, name='checkout_success'),
+    path('order_details/<order_number>', views.order_details, name='order_details'),
     # path('products/', views.BlogCategoryList.as_view(), name='manage_products'),
     # path('blog/', views.BlogCategoryList.as_view(), name='manage_blog'),
     # path('requests/', views.BlogCategoryList.as_view(), name='manage_requests'),
