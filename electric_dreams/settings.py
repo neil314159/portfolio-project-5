2d8c038cdc56abbedeb73a111757c23720997955
neil314159 <neil@neilboland.com>
2022-08-13 23:36:58 +0000

    Add auto-dismiss on bootstrap alerts for django messages, chaneg default product picture

--- electric_dreams/settings.py
+++ electric_dreams/settings.py
@@ -92,6 +92,14 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 
 # all-auth specific backend
 AUTHENTICATION_BACKENDS = [
