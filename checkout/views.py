1587f8fffac16aa9cb268a887b4d61f262436bd9
neil314159 <neil@neilboland.com>
2022-08-14 03:29:32 +0000

    Fixed checkout success function to work with custom user type

--- checkout/views.py
+++ checkout/views.py
@@ -10,7 +10,7 @@ from products.models import Product
 # from profiles.forms import UserProfileForm
 from profiles.models import CustomUser
 from bag.contexts import bag_contents
-
+from django.contrib.auth import get_user_model
 import stripe
 import json
 
@@ -140,15 +140,14 @@ def checkout_success(request, order_number):
 
         # Save the user's info
         if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
-            }
+            profile.default_phone_number = order.phone_number
+            default_country: order.country
+                # 'default_postcode': order.postcode,
+                # 'default_town_or_city': order.town_or_city,
+                # 'default_street_address1': order.street_address1,
+                # 'default_street_address2': order.street_address2,
+                # 'default_county': order.county,
+            
             # user_profile_form = UserProfileForm(profile_data, instance=profile)
             # if user_profile_form.is_valid():
             #     user_profile_form.save()
